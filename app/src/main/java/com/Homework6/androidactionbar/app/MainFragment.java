517009e9d6f5850267830b06ed1ff5ad190395e1
mpgard01 <mpgard01@louisville.edu>
2015-10-26 16:10:16 -0400

    Initial commit

---
+++ app/src/main/java/com/Homework6/androidactionbar/app/MainFragment.java
@@ -0,0 +1,27 @@
+package com.Homework6.androidactionbar.app;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        return view;
+    }
+
+}
+
